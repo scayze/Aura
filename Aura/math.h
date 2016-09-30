e413ba79eb76feffc39bec67f30bd52abe4d7c81
Manu <Manu@SCAYZE>
2016-09-30 14:46:52 +0200

    Math methods now inlined

--- Aura/math.h
+++ Aura/math.h
@@ -9,16 +9,16 @@ private:
 
 public:
 
-	static sf::Vector2f vectorNormalize(sf::Vector2f v);
-	static float vectorLength(sf::Vector2f v);
-	static float vectorDistance(sf::Vector2f v1, sf::Vector2f v2);
-	static float vectorSquaredDistance(sf::Vector2f v1, sf::Vector2f v2);
-	static sf::Vector2f vectorSetMagnitude(sf::Vector2f, float magnitude);
-	static float vectorDot(sf::Vector2f v1, sf::Vector2f v2);
-	static float vectorProjectOn(sf::Vector2f v1, sf::Vector2f axis);
-	static sf::Vector2f vectorRotateAroundPoint(sf::Vector2f p, sf::Vector2f pivot, float angle);
-	static float vectorGetRotation(sf::Vector2f p);
-	static float PI();
+	static inline sf::Vector2f vectorNormalize(sf::Vector2f v);
+	static inline float vectorLength(sf::Vector2f v);
+	static inline float vectorDistance(sf::Vector2f v1, sf::Vector2f v2);
+	static inline float vectorSquaredDistance(sf::Vector2f v1, sf::Vector2f v2);
+	static inline sf::Vector2f vectorSetMagnitude(sf::Vector2f, float magnitude);
+	static inline float vectorDot(sf::Vector2f v1, sf::Vector2f v2);
+	static inline float vectorProjectOn(sf::Vector2f v1, sf::Vector2f axis);
+	static inline sf::Vector2f vectorRotateAroundPoint(sf::Vector2f p, sf::Vector2f pivot, float angle);
+	static inline float vectorGetRotation(sf::Vector2f p);
+	static inline float PI();
 
 };
 
